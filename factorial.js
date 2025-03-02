38e1a745c9b0b1ec6f23c148167a9dab6b839553
NoraizRana <noraizrana389@gmail.com>
2025-03-02 10:14:09 +0500

    function to calculate factorial added

--- factorial.js
+++ factorial.js
@@ -1,10 +1,11 @@
 console.log("Tutorial 15");
 console.log("In this tutoruial we will calculate the factorial of given number");
 let a=7;
-function fact(f) {
-       f=1;
-    for (let i = 0; i <= f; i++) {
+function fact(n) {
+  let f=1;
+    for (let i = 1; i <= n; i++) {
         f=f*i;
     }
+    return f;
   }
-  console.log(fact(a));
+  console.log(fact(7));
